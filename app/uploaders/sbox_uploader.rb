9a0304ced26f1e27629f1896d9100442da8c4e70
HarenBroog <damian.romanow@gmail.com>
2014-06-02 12:19:35 +0200

    heroku config carr wave

--- app/uploaders/sbox_uploader.rb
+++ app/uploaders/sbox_uploader.rb
@@ -7,6 +7,9 @@ class SboxUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
+  end
   
   def extension_white_list
     %w(SBX)
